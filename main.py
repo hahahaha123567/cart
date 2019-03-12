7ab8a13a02e4ae26808f1ff4fadf79f65290aa73
Zhang Yaoxin <zhangyaoxin@yiwise.com>
2019-03-12 15:09:46 +0800

    1st version

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ class Node:
         return str(self.__dict__)
 
 
-
 def read_file(filename):
     file = pd.ExcelFile(filename)
     df_string = file.parse(0)
     data_set = np.array(df_string).tolist()  # type: list
     attributes = data_set[0]
+    attributes[0] = None
     data_set = data_set[1:]
     # ST1 -> ST, 正常1 -> 正常
     for data in data_set:
@@ -47,12 +47,13 @@ def create_tree(attributes, data_set):
         return Node(result=result_list.pop())
 
     # 如果遍历完所有特征，返回数据集中最多的类型
-    if len(attributes) == 0:
+    if len(set(attributes)) == 1:
         return Node(result='ST' if result_list.count('ST') > result_list.count('正常') else '正常')
 
     # 计算用来划分的属性和值
     cut_attribute, cut_value = calcu_cut_value(attributes, data_set)
     left_data_set, right_data_set = split_data_set(attributes, data_set, cut_attribute, cut_value)
+
     # 将划分过的属性置空
     attributes = attributes.copy()
     attributes[attributes.index(cut_attribute)] = None
@@ -62,16 +63,16 @@ def create_tree(attributes, data_set):
 
 
 def calcu_cut_value(attributes, data_set):
-    final_max_gini = -1
+    final_min_gini = 1
     final_cut_attribute = -1
     final_cut_value = -1
     # 对每个属性计算gini值
     for attribute in attributes[1:]:
-        attri_max_gini = -1
-        attri_cut_value = -1
         # 如果该属性为空(已经划分过即置空)则跳过
-        if attributes is None:
+        if attribute is None:
             continue
+        attri_min_gini = 1
+        attri_cut_value = -1
         # 本次循环中取data的第index个属性
         index = attributes.index(attribute)
         data_set.sort(key=(lambda x: x[index]))
@@ -80,7 +81,7 @@ def calcu_cut_value(attributes, data_set):
             cut_value = (data_set[i][index] + data_set[i + 1][index]) / 2
             #         <=   >
             # ST      a    c
-            # normal  b    d
+            # 正常    b    d
             a, b, c, d = 0, 0, 0, 0
             for t in range(len(data_set)):
                 if data_set[t][index] <= cut_value:
@@ -93,14 +94,14 @@ def calcu_cut_value(attributes, data_set):
                         c += 1
                     elif '正常' == data_set[t][0]:
                         d += 1
-            gini1 = 1 - (a * a + b * b) / ((a + b) * (a + b))
-            gini2 = 1 - (c * c + d * d) / ((c + d) * (c + d))
+            gini1 = 1 - 1.0 * (a * a + b * b) / ((a + b) * (a + b))
+            gini2 = 1 - 1.0 * (c * c + d * d) / ((c + d) * (c + d))
             gini = gini1 * (a + b) / (a + b + c + d) + gini2 * (c + d) / (a + b + c + d)
-            if gini > attri_max_gini:
-                attri_max_gini = gini
+            if gini < attri_min_gini:
+                attri_min_gini = gini
                 attri_cut_value = cut_value
-        if attri_max_gini > final_max_gini:
-            final_max_gini = attri_max_gini
+        if attri_min_gini < final_min_gini:
+            final_min_gini = attri_min_gini
             final_cut_attribute = attribute
             final_cut_value = attri_cut_value
     return final_cut_attribute, final_cut_value
@@ -108,6 +109,5 @@ def calcu_cut_value(attributes, data_set):
 
 if __name__ == "__main__":
     attributes, data_set = read_file('data.xls')
-
     tree = create_tree(attributes, data_set)
     print(tree.__repr__())
