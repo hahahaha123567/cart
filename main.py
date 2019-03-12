91f06064914fa0d14f679a09be2f0bb5d966a54b
Zhang Yaoxin <zhangyaoxin@yiwise.com>
2019-03-12 13:52:46 +0800

    with bug

--- main.py
+++ main.py
@@ -1,115 +1,113 @@
 import numpy as np
 import pandas as pd
 
-FLAG = 65535
-
 
 class Node:
-    def __init__(self, attribute=None, key=None, left=None, right=None):
-        self.attribute = attribute
-        self.key = key
-        self.left = left
-        self.right = right
+    def __init__(self, attribute=None, key=None, left=None, right=None, result=None):
+        self.attribute = attribute  # 非叶节点用来划分的属性
+        self.key = key  # 非叶节点用来划分属性的值
+        self.left = left  # 非叶节点的左子树
+        self.right = right  # 非叶节点的右子树
+        self.result = result  # 叶节点的结果
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
-def read_file():
-    file = pd.ExcelFile("data.xls")
+
+def read_file(filename):
+    file = pd.ExcelFile(filename)
     df_string = file.parse(0)
-    return np.array(df_string).tolist()
+    data_set = np.array(df_string).tolist()  # type: list
+    attributes = data_set[0]
+    data_set = data_set[1:]
+    # ST1 -> ST, 正常1 -> 正常
+    for data in data_set:
+        if data[0].startswith('ST'):
+            data[0] = 'ST'
+        else:
+            data[0] = '正常'
+    return attributes, data_set
 
 
-def create_tree(self, data_set, classes, feat_names):
-    """ 根据当前数据集递归创建决策树
-
-    :param self:
-    :param data_set: 数据集
-    :param feat_names: 数据集中数据相应的特征名称
-    :param classes: 数据集中数据相应的类型
-
-    """
-    # 如果数据集中只有一种类型停止树分裂
-    if len(set(classes)) == 1:
-        return classes[0]
-
-    # 如果遍历完所有特征，返回比例最多的类型
-    if len(feat_names) == 0:
-        return get_majority(classes)
-
-    # 分裂创建新的子树
-    tree = {}
-    best_feat_idx = self.choose_best_split_feature(data_set, classes)
-    feature = feat_names[best_feat_idx]
-    tree[feature] = {}
-
-    # 创建用于递归创建子树的子数据集
-    sub_feat_names = feat_names[:]
-    sub_feat_names.pop(best_feat_idx)
-
-    splited_dict = self.split_dataset(data_set, classes, best_feat_idx)
-    for feat_val, (sub_dataset, sub_classes) in splited_dict.items():
-        tree[feature][feat_val] = self.create_tree(sub_dataset,
-                                                   sub_classes,
-                                                   sub_feat_names)
-    self.tree = tree
-    self.feat_names = feat_names
-
-    return tree
+def split_data_set(attributes, data_set, attribute, key):
+    attribute_index = attributes.index(attribute)
+    left_data_set, right_data_set = [], []
+    for data in data_set:
+        target_data_set = left_data_set if data[attribute_index] > key else right_data_set
+        target_data_set.append(data)
+    return left_data_set, right_data_set
 
 
-def cut(df):
-    row_num = df.shape[0]
-    column_num = df.shape[1]
-    giniList = [0]
-    cutList = [0]
-    for column_index in range(column_num - 1)[1:]:
-        column = df.iloc[:, column_index][1:]
-        result = df.iloc[:, 0][1:]
-        gini, cut = calcuCutValue(column, result)
-        giniList.append(gini)
-        cutList.append(cut)
-    maxGini = max(giniList)
-    index = giniList.index(maxGini)
-    finalCutValue = cutList[index]
-    if finalCutValue != FLAG:
-        return Node(index, finalCutValue, cut(), cut())
-    else:
-        return Node()
+def create_tree(attributes, data_set):
+    result_list = list(map(lambda x: x[0], data_set))
+
+    # 如果数据集只剩一种类型, 返回该类型
+    if len(result_list) == 1:
+        return Node(result=result_list.pop())
+
+    # 如果遍历完所有特征，返回数据集中最多的类型
+    if len(attributes) == 0:
+        return Node(result='ST' if result_list.count('ST') > result_list.count('正常') else '正常')
+
+    # 计算用来划分的属性和值
+    cut_attribute, cut_value = calcu_cut_value(attributes, data_set)
+    left_data_set, right_data_set = split_data_set(attributes, data_set, cut_attribute, cut_value)
+    # 将划分过的属性置空
+    attributes = attributes.copy()
+    attributes[attributes.index(cut_attribute)] = None
+    left_node = create_tree(attributes, left_data_set)
+    right_node = create_tree(attributes, right_data_set)
+    return Node(cut_attribute, cut_value, left_node, right_node)
 
 
-def calcuCutValue(seriesWithoutSort, result):
-    seriesSorted = copy.deepcopy(seriesWithoutSort)
-    maxGini = 0
-    finalCutValue = FLAG
-    for i in range(len(seriesSorted))[1:]:
-        cutValue = (seriesSorted[i] + seriesSorted[i + 1]) / 2
-        #         <=   >
-        # ST      a    c
-        # normal  b    d
-        a, b, c, d = 0, 0, 0, 0
-        for t in range(len(seriesSorted - 1))[1:]:
-            if seriesSorted[t] <= cutValue:
-                if 'ST' in result[t]:
-                    a += 1
-                elif '正常' in result[t]:
-                    b += 1
-            else:
-                if 'ST' in result[t]:
-                    c += 1
-                elif '正常' in result[t]:
-                    d += 1
-        gini1 = 1 - (a * a + b * b) / ((a + b) * (a + b))
-        gini2 = 1 - (c * c + d * d) / ((c + d) * (c + d))
-        gini = gini1 * (a + b) / (a + b + c + d) + gini2 * (c + d) / (a + b + c + d)
-        if gini > maxGini:
-            maxGini = gini
-            finalCutValue = cutValue
-    return finalCutValue
+def calcu_cut_value(attributes, data_set):
+    final_max_gini = -1
+    final_cut_attribute = -1
+    final_cut_value = -1
+    # 对每个属性计算gini值
+    for attribute in attributes[1:]:
+        attri_max_gini = -1
+        attri_cut_value = -1
+        # 如果该属性为空(已经划分过即置空)则跳过
+        if attributes is None:
+            continue
+        # 本次循环中取data的第index个属性
+        index = attributes.index(attribute)
+        data_set.sort(key=(lambda x: x[index]))
+        for i in range(len(data_set) - 1):
+            # 本次循环中以第i个data和第i+1个data的平均值为分界
+            cut_value = (data_set[i][index] + data_set[i + 1][index]) / 2
+            #         <=   >
+            # ST      a    c
+            # normal  b    d
+            a, b, c, d = 0, 0, 0, 0
+            for t in range(len(data_set)):
+                if data_set[t][index] <= cut_value:
+                    if 'ST' == data_set[t][0]:
+                        a += 1
+                    elif '正常' == data_set[t][0]:
+                        b += 1
+                else:
+                    if 'ST' == data_set[t][0]:
+                        c += 1
+                    elif '正常' == data_set[t][0]:
+                        d += 1
+            gini1 = 1 - (a * a + b * b) / ((a + b) * (a + b))
+            gini2 = 1 - (c * c + d * d) / ((c + d) * (c + d))
+            gini = gini1 * (a + b) / (a + b + c + d) + gini2 * (c + d) / (a + b + c + d)
+            if gini > attri_max_gini:
+                attri_max_gini = gini
+                attri_cut_value = cut_value
+        if attri_max_gini > final_max_gini:
+            final_max_gini = attri_max_gini
+            final_cut_attribute = attribute
+            final_cut_value = attri_cut_value
+    return final_cut_attribute, final_cut_value
 
 
 if __name__ == "__main__":
-    data = read_file()
+    attributes, data_set = read_file('data.xls')
 
-    for l in data:
-        print(l)
-
-    # root = cut(df)
+    tree = create_tree(attributes, data_set)
+    print(tree.__repr__())
